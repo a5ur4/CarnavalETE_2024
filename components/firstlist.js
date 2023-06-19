b5f6c71b99538befa832386a4568dd5f48ba352e
Pedro Bastos <113993134+a5ur4@users.noreply.github.com>
2023-06-19 11:03:42 -0300

    Update FirstList to make phone calls.

--- components/firstlist.js
+++ components/firstlist.js
@@ -1,22 +1,28 @@
-import * as React from 'react';
+import React from 'react';
 import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import call from 'react-native-phone-call';
 
 const Firstlist = () => {
   const blocks = [
-    { id: 1, color: '#F9CD0F', text: '190', image: 'https://cdn-icons-png.flaticon.com/512/26/26300.png' },
-    { id: 2, color: '#F9CD0F', text: '192', image: 'https://cdn-icons-png.flaticon.com/512/1976/1976701.png' },
-    { id: 3, color: '#F9CD0F', text: '193', image: 'https://cdn-icons-png.flaticon.com/512/7341/7341048.png' },
+    { id: 1, color: '#F9CD0F', text: '190', image: 'https://cdn-icons-png.flaticon.com/512/26/26300.png', phoneNumber: '190' },
+    { id: 2, color: '#F9CD0F', text: '192', image: 'https://cdn-icons-png.flaticon.com/512/1976/1976701.png', phoneNumber: '192' },
+    { id: 3, color: '#F9CD0F', text: '193', image: 'https://cdn-icons-png.flaticon.com/512/7341/7341048.png', phoneNumber: '193' },
   ];
 
-  const handleBlockPress = (id) => {
-    console.log(`Block ${id} pressed!`);
+  const makePhoneCall = (phoneNumber) => {
+    const args = {
+      number: phoneNumber,
+      prompt: true,
+    };
+
+    call(args).catch(console.error);
   };
 
   return (
     <View>
       <Image
         source={{ uri: 'https://static.vecteezy.com/system/resources/previews/014/386/199/original/carnival-mask-illustration-png.png' }}
-        style={{ width: 75, height: 60, transform: [{ rotate: '-30deg' }], zIndex: 2,  top: -20, left: 10 }}
+        style={{ width: 75, height: 60, transform: [{ rotate: '-30deg' }], zIndex: 2, top: -20, left: 10 }}
       />
       <View style={styles.container}>
         {blocks.map((block) => (
@@ -24,7 +30,7 @@ const Firstlist = () => {
             <TouchableOpacity
               key={block.id}
               style={[styles.block, { backgroundColor: block.color }]}
-              onPress={() => handleBlockPress(block.id)}
+              onPress={() => makePhoneCall(block.phoneNumber)}
             >
               <Image source={{ uri: block.image }} style={styles.image} />
               <Text style={styles.text}>{block.text}</Text>
@@ -46,13 +52,13 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
     alignItems: 'center',
     paddingHorizontal: 10,
-    marginTop: -45
+    marginTop: -45,
   },
   block: {
     width: 85,
     height: 110,
     borderRadius: 10,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   image: {
     width: 50,
@@ -61,8 +67,8 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 17,
-    fontWeight: 'bold'
-  }
+    fontWeight: 'bold',
+  },
 });
 
 export default Firstlist;
